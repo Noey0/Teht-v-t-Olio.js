827ff00828b6cc26d66e8cdc24c1c55e4b187ec9
Noah <s2401156@edu.bc.fi>
2026-01-23 09:22:49 +0200

    Tehty

---
+++ Olio.js
@@ -0,0 +1,39 @@
+const opiskelija = {
+  nimi: "Nj",
+  ika: 19,
+  kurssi: "Ohjelmointi",
+};
+
+console.log(opiskelija.nimi);
+console.log(opiskelija.ika);
+console.log(opiskelija.kurssi);
+
+class Auto {
+  constructor(merkki, malli, vuosimalli) {
+    this.merkki = merkki;
+    this.malli = malli;
+    this.vuosimalli = vuosimalli;
+  }
+  tulostaTiedot() {
+    console.log(
+      `Merkki: ${this.merkki}, Malli: ${this.malli}, Vuosimalli: ${this.vuosimalli}`,
+    );
+  }
+}
+const auto1 = new Auto("Nissan", "Sentra", 2020);
+const auto2 = new Auto("Toyota", "Corolla", 2015);
+
+auto1.tulostaTiedot();
+auto2.tulostaTiedot();
+
+const kirjat = [
+  { nimi: "Blue Lock", kirjailija: "Muneyuki Kaneshiro", vuosi: 2018 },
+  { nimi: "The 48 Laws of Power", kirjailija: "Robert Greene", vuosi: 1998 },
+  { nimi: "Harry Potter", kirjailija: "J.K. Rowling", vuosi: 1997 },
+];
+
+kirjat.forEach((kirja) => {
+  console.log(
+    `Nimi: ${kirja.nimi}, Kirjailija: ${kirja.kirjailija}, Vuosi: ${kirja.vuosi}`,
+  );
+});
